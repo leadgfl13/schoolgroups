8ccf42d5abe0c5684229616166a2953ee4caad5b
leadgfl13 <leadgfl13@gmail.com>
2024-01-21 21:55:54 -0500

    Create period 9 array with students.  Make a button creates a button for each student with their name in it

--- scripts.js
+++ scripts.js
@@ -1,3 +1,22 @@
-document.addEventListener('click', ()=>{
-    alert('I work!')
+let body =document.body
+
+let period9 = document.createElement('button')
+period9.setAttribute('id', 'selectperiod')
+period9.innerHTML = 'Period 9'
+body.append(period9)
+period9.addEventListener('click', ()=>{
+    for(let i =0; i<period9.length; i++){
+        let card = document.createElement('button')
+        card.setAttribute('id','card')
+        body.append(card)
+        card.innerHTML = period9[i]
+    }
 })
+
+
+//need an array that holds all the students for a given period.
+// lets say period 9
+
+period9=['Jaden','Troy','Ariel', 'Alina','Jackson','Olivia','Toryn','Datavion','Candace','Jayda','Gabbie','Abbie','Callie','Jordan','Takayla','Emil'
+, 'Rodney']
+
