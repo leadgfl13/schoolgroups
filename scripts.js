9c274acc6f92b457d16be510e1f1b0b159e38261
leadgfl13 <leadgfl13@gmail.com>
2024-01-23 13:44:26 -0500

    Change makeButtons to take an array argument, so it can be used for either period, not just period 9

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
+//global variables
 let body =document.body
 let bottomleft=document.getElementById('bottomleft')
-
-function makebuttons(){
+//function for 
+function makebuttons(period){
     bottomright.innerHTML = ''
-    for(let i =0; i<period9.length; i++){
+    for(let i =0; i<period.length; i++){
         let card = document.createElement('button')
         card.setAttribute('id','card')
-        card.innerHTML = period9[i]
+        card.innerHTML = period[i]
         bottomright.append(card)
         card.addEventListener('click',()=>{
             period9.splice(i,1)
-            makebuttons()
+            makebuttons(period)
             console.log(period9)
 })}}
-
 
 // button to create period 9 roster
 let period9 = document.createElement('button')
@@ -21,35 +21,19 @@ period9.setAttribute('id', 'selectperiod')
 period9.innerHTML = 'Period 9'
 bottomleft.append(period9)
 period9.addEventListener('click', ()=>{
+    let p9 = true
     if(bottomright.innerHTML){
-        period9 =period9=['Jaden','Troy','Ariel', 'Alina','Jackson','Olivia','Toryn','Datavion','Candace','Jayda','Gabbie','Abbie','Callie','Jordan','Takayla','Emil'
+        period9 =['Jaden','Troy','Ariel', 'Alina','Jackson','Olivia','Toryn','Datavion','Candace','Jayda','Gabbie','Abbie','Callie','Jordan','Takayla','Emil'
         , 'Rodney']
-        makebuttons()
+        makebuttons(period9)
     }
     else{
-    makebuttons()
+    makebuttons(period9)
         }
+        return(p9)
     }
 )
 
-
-
-//button to create a clear roster
-
-let clear = document.createElement('button')
-clear.innerHTML ="Clear Roster"
-clear.setAttribute('id', 'selectperiod')
-bottomleft.append(clear)
-clear.addEventListener('click',()=>{
-    bottomright.innerHTML= ''
-    period9 =['Jaden','Troy','Ariel', 'Alina','Jackson','Olivia','Toryn','Datavion','Candace','Jayda','Gabbie','Abbie','Callie','Jordan','Takayla','Emil'
-    , 'Rodney']
-})
-
-
-
-//need an array that holds all the students for a given period.
-// lets say period 9
 
 period9=['Jaden','Troy','Ariel', 'Alina','Jackson','Olivia','Toryn','Datavion','Candace','Jayda','Gabbie','Abbie','Callie','Jordan','Takayla','Emil'
 , 'Rodney']
