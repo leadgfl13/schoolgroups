a9c7e44f5d2a2b53aba932d7e0b25b2946cf56ce
leadgfl13 <leadgfl13@gmail.com>
2024-01-23 10:02:46 -0500

    Add button to clear html of bottomright.  Also add a check on period 9 roster to only add roster if bottomright is empty

--- scripts.js
+++ scripts.js
@@ -1,23 +1,40 @@
 let body =document.body
-let bottomright=document.getElementById('bottomright')
+let bottomleft=document.getElementById('bottomleft')
 
+let bottomright=document.getElementById('bottomright')
+// button to create period 9 roster
 let period9 = document.createElement('button')
 period9.setAttribute('id', 'selectperiod')
 period9.innerHTML = 'Period 9'
-
-let bottomleft=document.getElementById('bottomleft')
 bottomleft.append(period9)
 period9.addEventListener('click', ()=>{
+    if(bottomright.innerHTML){
+        console.log("it's full")
+    }
+    else{
     for(let i =0; i<period9.length; i++){
         let card = document.createElement('button')
         card.setAttribute('id','card')
         card.addEventListener('click',()=>{
-            console.log(`hello, I am ${period9[i]}`)
+            card.remove()
         })
         bottomright.append(card)
         card.innerHTML = period9[i]
-    }
+    }}
 })
+
+
+
+//button to create a clear roster
+
+let clear = document.createElement('button')
+clear.innerHTML ="Clear Roster"
+clear.setAttribute('id', 'selectperiod')
+bottomleft.append(clear)
+clear.addEventListener('click',()=>{
+    bottomright.innerHTML= ''
+})
+
 
 
 //need an array that holds all the students for a given period.
