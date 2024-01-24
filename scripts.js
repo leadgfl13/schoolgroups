606eeab20a71fdfd6adfd74906266cce7a160163
leadgfl13 <leadgfl13@gmail.com>
2024-01-24 12:03:23 -0500

    Create function that takes an array as an argument and randomizes the elements of the array

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
-//global variables
+//--------------------global variables
 let body =document.body
 let bottomleft=document.getElementById('bottomleft')
 let period = 0
 let topright =document.getElementById('topright')
 
+
+//--------------------the rosters------------------------
 period_9=['Jaden','Troy','Ariel', 'Alina','Jackson','Olivia','Toryn','Datavion','Candace','Jayda','Gabbie','Abbie','Callie','Jordan','Takayla','Emil'
 , 'Rodney']
 
@@ -11,7 +13,7 @@ period_4 = ['Angelo','Landyn','Owen','Zach','John','Jackson','Matt','Amarah','Ki
 'Josiash','Jenna','Miranda','Damien','Gabe','Iziah','Neveah']
 
 
-//function for  making buttons depending on the period
+//--------------------function for making the student buttons depending on the period
 function makebuttons(period){
     bottomright.innerHTML = ''
     for(let i =0; i<period.length; i++){
@@ -25,7 +27,7 @@ function makebuttons(period){
             console.log(period)
 })}}
 
-//button for period 9
+//----------------button to create  period9 
 let period9 = document.createElement('button')
 period9.setAttribute('id', 'selectperiod')
 period9.innerHTML = 'Period 9'
@@ -40,10 +42,10 @@ period9.addEventListener('click', ()=>{
     else{
     makebuttons(period_9)
         }
-    }
-)
+    })
 
-// button to create period 4 roster
+
+//----------------button to create period 4
 let period4 = document.createElement('button')
 period4.setAttribute('id', 'selectperiod')
 period4.innerHTML = 'Period 4'
@@ -57,13 +59,10 @@ period4.addEventListener('click', ()=>{
     }
     else{
     makebuttons(period_4)
-        }
+ }
+    })
 
-    }
-
-)
-
-//check button just to make sure that THere is a record of which group is selected
+//--------check button just to make sure that there is a record of which group is selected
 let check = document.createElement('button')
 check.innerHTML = 'check'
 check.setAttribute('id', 'selectperiod')
@@ -80,19 +79,45 @@ check.addEventListener('click',()=>{
     }
 })
 
-//buttons for which grouping
+//---------------buttons for groupings
 let group2 = document.createElement('button')
 group2.setAttribute('id','groups')
 group2.innerHTML = 'Groups of 2'
 topright.append(group2)
+group2.addEventListener('click',makeGroup)
 
 
 let group3 = document.createElement('button')
 group3.setAttribute('id','groups')
 group3.innerHTML = 'Groups of 3'
 topright.append(group3)
+group3.addEventListener('click',makeGroup)
+
 
 let group4 = document.createElement('button')
 group4.setAttribute('id','groups')
 group4.innerHTML = 'Groups of 4'
 topright.append(group4)
+group4.addEventListener('click',makeGroup)
+
+
+//--------------create function to randomize roster
+
+function randomizer(anarray){
+    for(let i =anarray.length -1; i>0; i--){
+        const j = Math.floor(Math.random() * (i+1))
+        temp = anarray[i]
+        anarray[i] = anarray[j]
+        anarray[j] = temp
+    } 
+    return[anarray]
+}
+
+
+//------function for splitting up groups
+function makeGroup(groupsize){
+    if (period == 4){console.log(randomizer(period_4))
+    
+    
+}
+}
